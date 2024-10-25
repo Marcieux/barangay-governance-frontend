0e6671a24bf28b09112389ebb79b038e1743a4c3
Marcieux <marcj.doria@gmail.com>
2024-10-25 16:50:03 +0800

    Install Axios and Connect to Backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
+import axios from "axios";
 import Login from "./components/Login";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/")
+      .then((res) => {
+        console.log(res.data); // Should log: "Backend is connected!"
+      })
+      .catch((err) => console.error("Error connecting to backend:", err));
+  }, []);
+
   return (
     <div>
       <Login />
